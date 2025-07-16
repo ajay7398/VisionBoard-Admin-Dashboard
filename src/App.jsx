2132461de0534b5b8aeea6a6868e8d08635344be
Ajay kumar harijan <ajaykumar895377@gmail.com>
2025-07-16 10:17:40 +0530

    fixed importing path of importing DashBoardCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Header from './components/Header.jsx'
-import DashboardCards from './components/dashboardCards.jsx'
 import GraphCardSection from './components/graphCardSection.jsx'
 import Projects from './components/Projects.jsx'
 import Orders from './components/Orders.jsx'
 import Footer from './components/Footer.jsx'
+import DashboardCards from './components/dashboardCards.jsx'
 
 function App() {
    
